306b43efbba2e57fcfa2a9a2e64f1918cac3b7b9
camillepolice@icloud.com <Ap4hgfpaz.>
2024-11-16 19:05:21 +0100

    chore(conf): Update conf

--- tutteo-app/src/main.ts
+++ tutteo-app/src/main.ts
@@ -3,15 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlay, faPause, faForward, faBackward } from '@fortawesome/free-solid-svg-icons'
+
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
 
 const app = createApp(App)
 
+library.add(faPlay, faPause, faForward, faBackward)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
 app.use(createPinia())
 app.use(router)
 
-//app.use(PrimeVue)
+app.use(PrimeVue)
 
 app.mount('#app')
--- tutteo-app/vite.config.ts
+++ tutteo-app/vite.config.ts
@@ -5,12 +5,11 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': resolve(__dirname, './src'),
     },
   },
 })
